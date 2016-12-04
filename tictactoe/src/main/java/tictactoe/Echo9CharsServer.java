a6c7efd8d7310b483d6bc0899dab4ebbb50060b5
hazeltroost <hazelsteacup@gmail.com>
2016-12-04 00:28:25 -0600

    param length
    testing deployment

--- tictactoe/src/main/java/tictactoe/Echo9CharsServer.java
+++ tictactoe/src/main/java/tictactoe/Echo9CharsServer.java
@@ -14,8 +14,8 @@ public class Echo9CharsServer {
 			try {
 				String param = req.queryParams("content");
 				LOGGER.info(param);
-				//if (param.length() != 9)
-					//throw new IllegalArgumentException();
+				if (param.length() > 9)
+					throw new IllegalArgumentException();
 				return param.replaceAll("\\+", " ");
 			} catch (IllegalArgumentException e) {
 				res.status(400);
